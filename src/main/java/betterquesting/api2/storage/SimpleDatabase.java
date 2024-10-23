9692df369a4213d13b3d5a1128e1d06a3014808b
catsatori <163221322+catsatori@users.noreply.github.com>
2024-10-23 16:29:09 +0800

    傻逼bqu的dev不干活dc装死一周厂妹两小时就修好了诅咒这帮人生的小孩转生到GTNH宇宙当一辈子帕鲁拧一辈子螺丝

--- src/main/java/betterquesting/api2/storage/SimpleDatabase.java
+++ src/main/java/betterquesting/api2/storage/SimpleDatabase.java
@@ -1,31 +1,173 @@
 package betterquesting.api2.storage;
 
-import java.util.BitSet;
-import java.util.Collections;
-import java.util.List;
-import java.util.TreeMap;
+import it.unimi.dsi.fastutil.longs.LongCollection;
+import it.unimi.dsi.fastutil.longs.LongIterator;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
 
 public class SimpleDatabase<T> extends AbstractDatabase<T> {
 
-    private final BitSet idMap = new BitSet();
+    private final LongSet idMap = new LongSet() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public LongIterator iterator() {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public LongIterator longIterator() {
+            return null;
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(Long aLong) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(@NotNull Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(@NotNull Collection<? extends Long> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(@NotNull Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(@NotNull Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public boolean contains(long key) {
+            return false;
+        }
+
+        @Override
+        public long[] toLongArray() {
+            return new long[0];
+        }
+
+        @Override
+        public long[] toLongArray(long[] a) {
+            return new long[0];
+        }
+
+        @Override
+        public long[] toArray(long[] a) {
+            return new long[0];
+        }
+
+        @Override
+        public boolean add(long key) {
+            return false;
+        }
+
+        @Override
+        public boolean rem(long key) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(LongCollection c) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(LongCollection c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(LongCollection c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(LongCollection c) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(long k) {
+            return false;
+        }
+    };
 
     @Override
     public synchronized int nextID() {
-        return idMap.nextClearBit(0);
+        int id = 0;
+        while (idMap.contains(id)) {
+            id++;
+        }
+        return id;
     }
 
     @Override
     public synchronized DBEntry<T> add(int id, T value) {
         DBEntry<T> result = super.add(id, value);
         // Don't add when an exception is thrown
-        idMap.set(id);
+        idMap.add(id);
         return result;
     }
 
     @Override
     public synchronized boolean removeID(int key) {
         boolean result = super.removeID(key);
-        if (result) idMap.clear(key);
+        if (result) idMap.remove(key);
         return result;
     }
 
